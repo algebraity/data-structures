4578e8465f344e6e3703b016399df0b297afbb3b
Algebraity <algebraity@tutamail.com>
2026-06-21 10:31:11 -0400

    added graph outline:

--- data_structures/graph/graph/graph.h
+++ data_structures/graph/graph/graph.h
@@ -28,12 +28,35 @@ struct Graph {
 };
 
 /* ---------- Construct and free methods ---------- */
-
+Vertex* constructVertex(Value* val);
+Vertex* emptyVertex(void);
+Edge* constructEdge(Value* val1, Value* val2);
+Edge* emptyEdge(void);
+Graph* constructGraph(Vertex** vertices, Edge** edges);
+Graph* emptyGraph(void);
+void freeVertex(Vertex* v);
+void freeEdge(Edge* e);
+void freeGraph(Graph* graph);
 
 /* ---------- Basic graph operations ---------- */
-
+Value** graphElements(Graph* graph);
+int positions(Graph* graph);
+int graphSwap(Graph* graph, Vertex* v1, Vertex* v2);
+Vertex** vertices(Graph* graph);
+Edge** edges(Graph* graph);
+int removeVertex(Graph** graph, Vertex* v);
+int removeEdge(Graph** graph, Edge* e)
 
 /* ---------- Basic graph properties ---------- */
-
+size_t graphSize(Graph* graph);
+size_t numVertices(Graph* graph);
+size_t numEdges(Graph* graph);
+int isVertexInGraph(Graph* graph, Vertex* v);
+int isEdgeInGraph(Graph* graph, Edge* e);
+int areAdjacent(Vertex* v1, Vertex* v2);
+int isTwoSidedEdge(Edge* e1, Edge* e2);
+int isTwoSidedGraph(Graph* g);
+int isEmpty(Graph* graph);
+int isTree(Graph* graph);
 
 #endif
